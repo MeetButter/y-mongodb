621fcbd4c288f923556d4f37ed4b18e3768c16ce
Olamilekan Ibrahim <leksyib14@gmail.com>
2023-06-07 11:46:10 +0400

    Optimization: remove Y.applyUpdate loop, use Y.mergeUpdates to join all updates together then apply all as one update

--- src/y-mongodb.js
+++ src/y-mongodb.js
@@ -1,60 +1,64 @@
-import * as Y from 'yjs'
-import * as binary from 'lib0/binary.js'
-import * as promise from 'lib0/promise.js'
+import * as Y from "yjs";
+import * as binary from "lib0/binary.js";
+import * as promise from "lib0/promise.js";
 //
-import { MongoAdapter } from './mongo-adapter'
-import * as U from './utils'
+import { MongoAdapter } from "./mongo-adapter";
+import * as U from "./utils";
 
-const getUpdates = docs => {
-  if (!Array.isArray(docs) || !docs.length) return []
+const getUpdates = (docs) => {
+  if (!Array.isArray(docs) || !docs.length) return [];
 
-  return docs.map(update => update.value.buffer)
-}
+  return docs.map((update) => update.value.buffer);
+};
 
 export class MongodbPersistence {
   /**
    * @param {string} location
    * @param {string} [collection]
    */
-  constructor (location, collection) {
-    const db = new MongoAdapter(location, collection)
-    this.tr = promise.resolve()
+  constructor(location, collection) {
+    const db = new MongoAdapter(location, collection);
+    this.tr = promise.resolve();
 
-    this._transact = f => {
-      const currTr = this.tr
+    this._transact = (f) => {
+      const currTr = this.tr;
       this.tr = (async () => {
-        await currTr
-        let res = /** @type {any} */ (null)
+        await currTr;
+        let res = /** @type {any} */ (null);
         try {
-          res = await f(db)
+          res = await f(db);
         } catch (err) {
-          console.warn('Error during saving transaction', err)
+          console.warn("Error during saving transaction", err);
         }
-        return res
-      })()
-      return this.tr
-    }
+        return res;
+      })();
+      return this.tr;
+    };
   }
 
   /**
    * @param {string} docName
    * @return {Promise<Y.Doc>}
    */
-  getYDoc (docName) {
-    return this._transact(async db => {
-      const docs = await U.getMongoUpdates(db, docName)
-      const updates = getUpdates(docs)
-      const ydoc = new Y.Doc()
-      ydoc.transact(() => {
-        for (let i = 0; i < updates.length; i++) {
-          Y.applyUpdate(ydoc, updates[i])
-        }
-      })
+  getYDoc(docName) {
+    return this._transact(async (db) => {
+      const docs = await U.getMongoUpdates(db, docName);
+      const updates = getUpdates(docs);
+      const ydoc = new Y.Doc();
+
+      const merge = Y.mergeUpdates(updates);
+      Y.applyUpdate(ydoc, merge);
+
       if (updates.length > U.PREFERRED_TRIM_SIZE) {
-        await U.flushDocument(db, docName, Y.encodeStateAsUpdate(ydoc), Y.encodeStateVector(ydoc))
+        await U.flushDocument(
+          db,
+          docName,
+          Y.encodeStateAsUpdate(ydoc),
+          Y.encodeStateVector(ydoc)
+        );
       }
-      return ydoc
-    })
+      return ydoc;
+    });
   }
 
   /**
@@ -62,37 +66,37 @@ export class MongodbPersistence {
    * @param {Uint8Array} update
    * @return {Promise<number>} Returns the clock of the stored update
    */
-  storeUpdate (docName, update) {
-    return this._transact(db => U.storeUpdate(db, docName, update))
+  storeUpdate(docName, update) {
+    return this._transact((db) => U.storeUpdate(db, docName, update));
   }
 
   /**
    * @param {string} docName
    * @return {Promise<void>}
    */
-  clearDocument (docName) {
-    return this._transact(async db => {
-      await db.del(U.createDocumentStateVectorKey(docName))
-      await U.clearUpdatesRange(db, docName, 0, binary.BITS32)
-    })
+  clearDocument(docName) {
+    return this._transact(async (db) => {
+      await db.del(U.createDocumentStateVectorKey(docName));
+      await U.clearUpdatesRange(db, docName, 0, binary.BITS32);
+    });
   }
 
   /**
    * @param {string} docName
    * @return {Promise<void>}
    */
-  flushDocument (docName) {
-    return this._transact(async db => {
-      const docs = await U.getMongoUpdates(db, docName)
-      const updates = getUpdates(docs)
-      const { update, sv } = U.mergeUpdates(updates)
-      await U.flushDocument(db, docName, update, sv)
-    })
+  flushDocument(docName) {
+    return this._transact(async (db) => {
+      const docs = await U.getMongoUpdates(db, docName);
+      const updates = getUpdates(docs);
+      const { update, sv } = U.mergeUpdates(updates);
+      await U.flushDocument(db, docName, update, sv);
+    });
   }
 
-  flushDB () {
-    return this._transact(async db => {
-      await U.flushDB(db)
-    })
+  flushDB() {
+    return this._transact(async (db) => {
+      await U.flushDB(db);
+    });
   }
 }
